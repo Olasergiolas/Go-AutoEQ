c69d0cd18a901161e0a0dee3f445299d5c3fae0b
Sergio García <Olasergiolas@gmail.com>
2021-10-22 13:58:12 +0200

    Añadir constantes faltantes
    Además, evitar mezclar inglés y español en el código.

--- pkg/Banda.go
+++ pkg/Banda.go
@@ -1,9 +1,20 @@
 package AutoEQ
 
-type Tipo string
+type BandType string
 
 const (
-	Bell Tipo = "Bell"
+	Bell      BandType = "Bell"
+	HighPass  BandType = "High Pass"
+	HighShelf BandType = "High Shelf"
+	LowPass   BandType = "Low Pass"
+	LowShelf  BandType = "Low Shelf"
+	Notch     BandType = "Notch"
+	Resonance BandType = "Resonance"
+
+	DefaultMode      string = "RLC (BT)"
+	DefaultSlope     string = "x1"
+	DefaultSilenced  bool   = false
+	DefaultExclusive bool   = false
 )
 
 type banda struct {
@@ -14,5 +25,5 @@ type banda struct {
 	quality   float32
 	exclusive bool
 	slope     string
-	band_type Tipo
+	band_type BandType
 }
