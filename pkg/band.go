1649ba56448da866f04a1721bce51f8c46bc2e1b
Sergio García <Olasergiolas@gmail.com>
2021-10-22 14:18:34 +0200

    Añadir rangos de valores para banda
    Estos son los rangos de valores permitidos en EasyEffects para una banda. Con esto
    queda completada la estructura para almacenar una banda y las constantes necesarias.
    
    Closes #8, closes #10, closes #12.

--- pkg/band.go
+++ pkg/band.go
@@ -15,6 +15,13 @@ const (
 	DefaultSlope     string = "x1"
 	DefaultSilenced  bool   = false
 	DefaultExclusive bool   = false
+
+	MaxFreq    float32 = 24000.0
+	MinFreq    float32 = 10.0
+	MaxGain    float32 = 20.0
+	MinGain    float32 = -20.0
+	MaxQuality float32 = 100.0
+	MinQuality float32 = 0.0
 )
 
 type banda struct {
