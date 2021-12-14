947bbc16ed9df800ca94b7a979f5c57c68b1516e
Sergio García <sergiogc5@correo.ugr.es>
2021-12-14 20:27:50 +0100

    Soporte para logs en archivo #49
    Se incluye la función prepareLogFile para gestionar la apertura/creación del fichero de logs. Además, se añaden dos nuevos eventos a registrar.

--- pkg/autoeq/log_wrapper.go
+++ pkg/autoeq/log_wrapper.go
@@ -26,6 +26,8 @@ var (
 	fileNotOpenedEvent     = &ErrorEvent{1, "couldn't open the requested file: %s"}
 	configNotExportedEvent = &ErrorEvent{2, "couldn't export the config profile file: %s"}
 	invalidArgRangeEvent   = &ErrorEvent{3, "argument %s is out of expected bounds"}
+	missingEnvVarEvent     = &ErrorEvent{4, "env variable %s does not exist"}
+	configInfoEvent        = &ErrorEvent{5, "var %s: value %s"}
 )
 
 func newWrapper(logger *logrus.Logger) *MyLogger {
@@ -35,8 +37,11 @@ func newWrapper(logger *logrus.Logger) *MyLogger {
 func newBaseLogger() *logrus.Logger {
 	logger := logrus.New()
 	logger.SetFormatter(&logrus.JSONFormatter{})
-	logrus.SetOutput(os.Stdout)
+	f, err := prepareLogFile()
 
+	if err == nil {
+		logger.SetOutput(f)
+	}
 	return logger
 }
 
@@ -45,6 +50,17 @@ func NewLogger() *MyLogger {
 	wrapper := newWrapper(baseLogger)
 
 	return wrapper
+}
+
+func prepareLogFile() (*os.File, error) {
+	path := GetLogPath()
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
 }
 
 func (l *MyLogger) SuccessLog(context string) {
@@ -61,4 +77,12 @@ func (l *MyLogger) ConfigNotExportedLog(path string) {
 
 func (l *MyLogger) InvalidArgRangeLog(arg string) {
 	l.WithFields(defaultFields).Warnf(invalidArgRangeEvent.msg, arg)
+}
+
+func (l *MyLogger) EnvVarMissingLog(envVar string) {
+	l.WithFields(defaultFields).Infof(missingEnvVarEvent.msg, envVar)
+}
+
+func (l *MyLogger) ConfigInfoLog(ident, value string) {
+	l.WithFields(defaultFields).Infof(configInfoEvent.msg, ident, value)
 }
